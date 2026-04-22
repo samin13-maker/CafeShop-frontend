4067e4fdc1aafd77a5b625765253b26f67756d2e
samin13-maker <243727@ids.upchiapas.edu.mx>
2026-04-22 03:11:53 -0600

    Configuraciones

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
 });
 
-// Agrega el token automáticamente a cada petición si existe
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
